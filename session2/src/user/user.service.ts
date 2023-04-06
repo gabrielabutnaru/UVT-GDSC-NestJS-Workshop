6e22ffed51c61d0fb2f52745a4b6fde51960fd40
Damian Beles <damianteodor.beles@gmail.com>
2023-04-06 18:54:27 +0300

    Add type

--- session2/src/user/user.service.ts
+++ session2/src/user/user.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {User} from "./user.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepo) {}
+  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
 
   create(user) {
-    const newUser = this.userRepo.create(user)
+    const newUser = this.userRepo.create(user);
 
     return this.userRepo.save(newUser);
   }
